de2d7ac1c7a6ea7e9a5ba5b4e535c027397af1da
masx200 <masx200@qq.com>
2025-03-27 11:32:23 +0800

    Update fresh.gen.ts

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -8,7 +8,7 @@ import * as $api_joke from "./routes/api/joke.ts";
 import * as $greet_name_ from "./routes/greet/[name].tsx";
 import * as $index from "./routes/index.tsx";
 import * as $Counter from "./islands/Counter.tsx";
-import { type Manifest } from "$fresh/server.ts";
+import type { Manifest } from "$fresh/server.ts";
 
 const manifest = {
     routes: {
